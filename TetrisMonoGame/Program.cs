d3054828cff35844806d8542a4944d8b2b772916
Parzival2066931 <tomy1421630@outlook.com>
2025-10-30 14:44:44 -0400

    premier commit

---
+++ TetrisMonoGame/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new TetrisMonoGame.Game1();
+game.Run();
---
+++ TetrisMonoGame/Tetris.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisMonoGame
+{
+    class Tetris
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private int _rot;
+        public int Rot
+        {
+            get => _rot;
+            set => _rot = (value + 4) % 4;
+        }
+        public PieceType Type { get; private set; }
+        private int[,] _shape;
+
+    }
+}
